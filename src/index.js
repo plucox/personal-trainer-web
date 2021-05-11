8eda33152aa4f13dfd053c8548b26b23e5850f09
Exeven <exeven15@gmail.com>
2021-05-11 13:07:14 +0200

    Added simple dashboard

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Paperbase from './views/Paperbase';
+
+
+
+const App = () => {
+  return(
+    <div>
+    <Paperbase />
+    </div>
+  );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
